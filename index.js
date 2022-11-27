b1880ce04736b0754a31a6ad2f619d5b021db242
son189 <son.august.7334@gmail.com>
2022-11-28 00:43:20 +0300

    complete assignments

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+const main =document.querySelector('#main');
+
+main.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory'
+
+newHeader.innerHTML = "Jiji AL Quid is the champion"
+
+document.body.append(newHeader);
